81a906a135b51523c352fb2befc39b47464ed443
Joey Whalen <jtktwhalen@gmail.com>
2021-02-06 14:29:31 -0500

    update Library to print hours

--- src/javaLibraryLabTTSHW/Library.java
+++ src/javaLibraryLabTTSHW/Library.java
@@ -1,27 +1,43 @@
 package javaLibraryLabTTSHW;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Library {
+	private static PrintStream printOpeningHours;
 	// Add the missing implementation to this class
-//	ArrayList<String> firstLibrary = new ArrayList<String>();
-//	ArrayList<String>secondLibrary = new ArrayList<String>();
-//
-//    public static void main(String[] args) {
-//        // Create two libraries
-//        Library firstLibrary = new Library("10 Main St.");
-//        Library secondLibrary = new Library("228 Liberty St.");
-//
-//        // Add four books to the first library
-//        firstLibrary.addBook(new Book("The Da Vinci Code"));
-//        firstLibrary.addBook(new Book("Le Petit Prince"));
-//        firstLibrary.addBook(new Book("A Tale of Two Cities"));
-//        firstLibrary.addBook(new Book("The Lord of the Rings"));
-//
-//        // Print opening hours and the addresses
-//        System.out.println("Library hours:");
-//        printOpeningHours();
-//        System.out.println();
+	ArrayList<String> firstLibrary = new ArrayList<String>();
+	ArrayList<String>secondLibrary = new ArrayList<String>();
+
+    public Library(String string) {
+		// TODO Auto-generated constructor stub
+	}
+    
+    private void addBook(Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    private static void printOpeningHours() {
+		// TODO Auto-generated method stub
+		printOpeningHours = System.out.printf("Libraries are open daily from 9am to 5pm.");//String printOpeningHours = "Libraries are open...";
+	}
+
+	public static void main(String[] args) {
+        // Create two libraries
+        Library firstLibrary = new Library("10 Main St.");
+        Library secondLibrary = new Library("228 Liberty St.");
+
+        // Add four books to the first library
+        firstLibrary.addBook(new Book("The Da Vinci Code"));
+        firstLibrary.addBook(new Book("Le Petit Prince"));
+        firstLibrary.addBook(new Book("A Tale of Two Cities"));
+        firstLibrary.addBook(new Book("The Lord of the Rings"));
+
+        // Print opening hours and the addresses
+        System.out.println("Library hours:");
+        printOpeningHours();//System.out.println(firstLibrary.printOpeningHours());
+        System.out.println();
 //
 //        System.out.println("Library addresses:");
 //        firstLibrary.printAddress();
@@ -51,6 +67,7 @@ public class Library {
 //        // Print the titles of available from the first library
 //        System.out.println("Books available in the first library:");
 //        firstLibrary.printAvailableBooks();
-//  }
-	
+  }
+
+		
 }
